5d936e3ccfd6de327e5ec07a37010da9394cac14
Stefanos <stefanosu422@gmail.com>
2019-09-30 18:37:30 -0400

    seeded database

--- my_backend_api/db/seeds.rb
+++ my_backend_api/db/seeds.rb
@@ -7,10 +7,25 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+
+
 User.create(username:'Stefanos', password_digest:'abc123', subchannel_id:1, post_id:1, comment_id:1)
+
+User.create(username:'Stefan', password_digest:'123abc', subchannel_id:2, post_id:2, comment_id:2)
+
 
 Subchannel.create(channel_name:'Tech', post_id: 1, user_id:1)
 
+Subchannel.create(channel_name:'Food', post_id: 1, user_id:2)
+
+
 Post.create(title: 'Almost done', text:'first_post', url:'digitalocean.com', comment_id: 1, user_id:1)
 
+Post.create(title: 'African Food', text:'Shiro', url:'massawa.com', comment_id: 1, user_id:2)
+
+
 Comment.create(text:'JS is weird', user_id:1)
+
+Comment.create(text:'food is good', user_id:2)
+
+
