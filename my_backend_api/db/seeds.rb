875b760d4960b68a3fa3f48316ddbe9b25f22e67
Stefanos <stefanosu422@gmail.com>
2019-10-01 21:58:21 -0400

    modified seed file

--- my_backend_api/db/seeds.rb
+++ my_backend_api/db/seeds.rb
@@ -10,22 +10,32 @@
 
 
 User.create(username:'Stefanos', password_digest:'abc123', subchannel_id:1, post_id:1, comment_id:1)
+User.create(username:'Mike', password_digest:'123abc', subchannel_id:2, post_id:2, comment_id:1)
+User.create(username:'Mus', password_digest:'123abc', subchannel_id:1, post_id:2, comment_id:1)
+User.create(username:'Amman', password_digest:'123abc', subchannel_id:3, post_id:2, comment_id:1)
+User.create(username:'Bear', password_digest:'123abc', subchannel_id:5, post_id:1, comment_id:1)
 
-User.create(username:'Stefan', password_digest:'123abc', subchannel_id:2, post_id:2, comment_id:2)
 
 
 Subchannel.create(channel_name:'Tech', post_id: 1, user_id:1)
-
 Subchannel.create(channel_name:'Food', post_id: 1, user_id:2)
+Subchannel.create(channel_name:'Basketball', post_id: 1, user_id:3)
+Subchannel.create(channel_name:'Sports', post_id: 1, user_id:2)
+Subchannel.create(channel_name:'Politics', post_id: 1, user_id:4)
+Subchannel.create(channel_name:'Poetry', post_id: 1, user_id:5)
 
 
 Post.create(title: 'Almost done', text:'first_post', url:'digitalocean.com', comment_id: 1, user_id:1)
-
-Post.create(title: 'African Food', text:'Shiro', url:'massawa.com', comment_id: 1, user_id:2)
+Post.create(title: 'Eritrean Food', text:'Shiro', url:'massawa.com', comment_id: 1, user_id:3)
+Post.create(title: 'Sports ', text:'Soccor', url:'espn.com', comment_id: 1, user_id:3)
+Post.create(title: 'Tradgey', text:'Mahmoud Darwish', url:'poetry.com', comment_id: 1, user_id:1)
+Post.create(title: '2020Race', text:'', url:'nytimes.com', comment_id: 1, user_id:5)
 
 
 Comment.create(text:'JS is weird', user_id:1)
-
-Comment.create(text:'food is good', user_id:2)
+Comment.create(text:'Knicks...', user_id:5)
+Comment.create(text:'Bernie Pandas', user_id:3)
+Comment.create(text:'Water', user_id:1)
+Comment.create(text:'food is good', user_id:3)
 
 
