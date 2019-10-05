7b02b70b1f11348421092aee7f4b01b5e3b40c7b
Stefanos <stefanosu422@gmail.com>
2019-10-04 23:25:07 -0400

    added reducers

--- refugee-forum/src/index.js
+++ refugee-forum/src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware)
+
+ReactDOM.render(<Provider> 
+    <App store={store}/>, document.getElementById('root'),
+    </Provider>) 
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
