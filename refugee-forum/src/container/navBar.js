d7d5ffc098e939dfcb41d51c030c637e8ba3f0a7
Stefanos <stefanosu422@gmail.com>
2019-11-04 15:19:21 -0500

    added functionality

--- refugee-forum/src/container/navBar.js
+++ refugee-forum/src/container/navBar.js
@@ -3,22 +3,21 @@ import SearchBar from './SearchBar';
 import SignUp from './SignUp';
 import LogIn from '../presentational/LogIn';
 import UserSettings from '../presentational/UserSettings';
-import MyProfile from '../presentational/MyProfile'
 import LogOut from '../presentational/LogOut';
 
 class NavBar extends React.Component {
     
-
     render() { 
-        return ( <div>
-            Navbar
-            make clickable SignUp icon for this on navBar// <SignUp/> 
-            make clickable LogIn icon for this on navBar//<LogIn/>
-            make SearchBar form for this on the Searchbar component that will be displayed on Navbar<SearchBar/>
-            make clickable MyProfile icon for this on navBar// <MyProfile/>
-            make clickable UserSettings icon for this on navBar// <UserSettings/>
-            make clickable LogOut icon for this on navBar// <LogOut/>
-            </div>);
+        return (
+            <div>
+                Navbar
+                make clickable SignUp icon for this on navBar// <SignUp/> 
+                make clickable LogIn icon for this on navBar//<LogIn />
+                make SearchBar form for this on the Searchbar component that will be displayed on Navbar<SearchBar/>
+                make clickable UserSettings icon for this on navBar// <UserSettings/>
+                make clickable LogOut icon for this on navBar// <LogOut/>
+                </div>
+        );
     }
 }
 export default NavBar;
--- refugee-forum/src/presentational/myProfile.js
+++ refugee-forum/src/presentational/myProfile.js
@@ -1,30 +1,30 @@
-import React from 'react';
+// import React from 'react';
 
 
-class MyProfile extends React.Component {
+// class MyProfile extends React.Component {
 
-    state = {
-        username: ''
-    }
+//     state = {
+//         username: ''
+//     }
 
-    componentDidMount() {
-        fetch('http://localhost:3000/myprofile', {
-            headers: {
-                Authorization: localStorage.token 
-            }
-        })
-            .then(resp => resp.json())
-            .then(profileData => {
-                this.setState({username: profileData.username});
-            })
-        }
+//     componentDidMount() {
+//         fetch('http://localhost:3000/myprofile', {
+//             headers: {
+//                 Authorization: localStorage.token 
+//             }
+//         })
+//             .then(resp => resp.json())
+//             .then(profileData => {
+//                 this.setState({username: profileData.username});
+//             })
+//         }
 
-    render() { 
-        return ( 
-            <div>
-            Welcome, {this.state.username}
-            </div> );
-    }
-}
+//     render() { 
+//         return ( 
+//             <div>
+//             Welcome, {this.state.username}
+//             </div> );
+//     }
+// }
 
-export default MyProfile;
+// export default MyProfile;
