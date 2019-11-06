c692f47b815569cc9a26ba6f9fb935e69a687ec4
Joseph G Syverson <josephgavinsyverson@gmail.com>
2019-11-06 14:33:25 -0500

    Fix import file path for component LogOut

--- refugee-forum/src/container/navBar.js
+++ refugee-forum/src/container/navBar.js
@@ -3,15 +3,15 @@ import SearchBar from './SearchBar';
 import SignUp from './SignUp';
 import LogIn from '../presentational/LogIn';
 import UserSettings from '../presentational/UserSettings';
-import LogOut from '../presentational/LogOut';
+import LogOut from './presentational/LogOut';
 
 class NavBar extends React.Component {
-    
-    render() { 
+
+    render() {
         return (
             <div>
                 Navbar
-                make clickable SignUp icon for this on navBar// <SignUp/> 
+                make clickable SignUp icon for this on navBar// <SignUp/>
                 make clickable LogIn icon for this on navBar//<LogIn />
                 make SearchBar form for this on the Searchbar component that will be displayed on Navbar<SearchBar/>
                 make clickable UserSettings icon for this on navBar// <UserSettings/>
