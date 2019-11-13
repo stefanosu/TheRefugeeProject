f949b364b486231e824f4487c4148d5795021ae0
Stefanos <stefanosu422@gmail.com>
2019-11-13 14:57:41 -0500

    added CRUD action requests for subchannel

--- my_backend_api/config/routes.rb
+++ my_backend_api/config/routes.rb
@@ -1,12 +1,25 @@
 Rails.application.routes.draw do
+  # CRUD REQUESTS FOR USERS LOGIIN/LOGOUT AUTH AND OAUTH#
   post 'api/v1/login', to: 'api/v1/sessions#login'
-  get 'api/v1/get_current_user', to: 'api/v1/sessions#get_current_user'
-  post '/signup', to: 'api/v1/users#create'
+  get  'api/v1/get_current_user', to: 'api/v1/sessions#get_current_user'
+  post 'signup', to: 'api/v1/users#create'
+  
+
+  # CRUD REQUESTS FOR USERS SUBCHANNELS#
+  post 'api/v1/makeChannel', to: 'api/v1/subchannels#create'
+  get  'api/v1/getCurrentChannel', to: 'api/v1/subchannels#show'
+  delete 'api/v1/destroyChannel', to: 'api/v1/subchannels#destroy'
+  patch 'api/v1/udpateChannel', to: 'api/v1/subchannels#update'
+
+# CRUD REQUESTS FOR USERS POSTS ON SUBCHANNELS#
+
+
+# CRUD REQUESTS FOR USERS COMMENTS ON POSTS#
 
   namespace :api do 
     namespace :v1 do 
       resources :users
-      resources :subchannel
+      resources :subchannels
       resources :posts
       resources :comments
     end
